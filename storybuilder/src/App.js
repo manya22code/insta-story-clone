939b74ef5b019efaf6cac13d6ffc9d1c27a90d37
Your Name <you@example.com>
2024-06-28 15:49:00 +0530

    adding files

---
+++ storybuilder/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Stories from 'react-insta-stories';
+function App() {
+
+  const story = [ "storybuilder/public/assets/94c32ab12a69c175f8152d81ab27ab978c48a204.mp4",
+    
+
+  ]
+  return (
+    <div className="App">
+      <Stories
+      stories={story}
+      />
+      
+    </div>
+  );
+}
+
+export defa
